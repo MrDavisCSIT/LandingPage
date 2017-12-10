87dacbef0a446a077a1d6b9322102c2546451ba8
Luke Tainton <luke@taintonfamily.co.uk>
2017-12-10 20:13:39 +0000

    Start site

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  $title = "Intelligence Not Found"
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php include 'scripts/head.php' ?>
+  </head>
+  <body>
+    <div id="divContent" class="container-fluid">
+      <h1 id="h1Title">Intelligence Not Found</h1>
+      <p id="pCopyright">&copy; Intelligence Not Found, 2017.</p> <!-- Copyright Footer -->
+    </div>
+    <?php include 'scripts/footer.php' ?>
+  </body>
+</html>
---
+++ scripts/head.php
@@ -0,0 +1,4 @@
+<title><?php echo($title); ?></title>
+<link rel="stylesheet" href="styles/index.css">
+<link rel="shortcut icon" href="img/favicon.ico">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
